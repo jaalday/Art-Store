bb35003d62c2e486835c941d9f0e9e55681a34e2
Jjaclyn Alday <jaalday123@gmail.com>
2024-06-25 20:47:53 -0400

    ahh

--- shop-frontend/src/Pages/Cart.jsx
+++ shop-frontend/src/Pages/Cart.jsx
@@ -19,9 +19,9 @@ export const Cart = () => {
             if (cartItems[product.id] > 0) {
                 return (
                    
-                    <div key={product.id} className="cartItem">
+                    <div key={product.id} className="product-card">
                         <h3>{product.name}</h3>
-                        <img className="cartImg" src={product.img} alt={product.name}/>
+                        <img className="product-image" src={product.image} alt={product.name}/>
                         <p>Price: ${product.price}</p>
                         <p>Quantity: {cartItems[product.id]}</p>
                         <div className="countHandler">
